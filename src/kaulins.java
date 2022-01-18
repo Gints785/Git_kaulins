b01be0af75d3279e1bef45aba97f36ef6bd1eefd
Kanders <gints.kanders@inbox.lv>
2022-01-18 10:44:26 +0200

    Papildinats ar ciklu

--- src/kaulins.java
+++ src/kaulins.java
@@ -1,21 +1,21 @@
 import java.util.Random;
 
 public class kaulins {
-	static void mestkaulinu(){
+	static void mestkaulinu(int reizes){
 
 	
 		
-		int skaitli;
-		Random rand = new Random();
 		
-		skaitli = rand.nextInt(6)+1;
+		Random rand = new Random();
+		for(int i=1;i<=reizes;i++){
+		int skaitli = rand.nextInt(6)+1;
 		System.out.println("Uzkrita : "+skaitli);
 
 	}
-		
+	}	
 		
 	public static void main(String[] args) {
-		mestkaulinu();
+		mestkaulinu(3);
 	}
 
 }
