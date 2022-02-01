554110c66f9387e5ab723b87eab54dd9cf348149
kaaaaaaaaaaarl <caalis18@gmail.com>
2022-02-01 13:50:30 +0200

    jaunakopija

--- src/kaulins.java
+++ src/kaulins.java
@@ -1,25 +1,63 @@
 import java.util.Random;
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class kaulins {
-	static void mestkaulinu(int reizes){
-		Random rand = new Random();
-		
+	static int mestkaulinu(){
+		Random rand = new Random();		
 		int skaitli;
-		for(int i=1;i<=reizes;i++){
 		skaitli = rand.nextInt(6)+1;
-		System.out.println("Uzkrita : "+skaitli);
-		}
+		return skaitli;
+		
 	}
 		
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		int reizes;
+		int reizes, izvele, lielakais=0;
+		
+		
 		do{
-		System.out.println("cik reizes mest kaulinu");
-		reizes = scan.nextInt();
-		}while(reizes<1);
-		mestkaulinu(reizes);
+			
+		System.out.println("1-Mest kaulinu | 2-apkatit pedejo metienu" + "|3-apskatit llielako \4-apturet");
+		izvele = scan.nextInt();
+		switch(izvele){
+		case 1: 
+			do{
+				System.out.println("cik reizes mest?");
+				reizes = scan.nextInt();
+			
+			}while(reizes<1);
+			for(int i=0; i<rezes; i++){
+				pedejie.add(mestkaulinu());
+			}
+		break;
+		
+		case 2:
+			System.out.println("pedeja metiena sk ir ");
+			for(Integer i : pedejie){
+				System.out.print(i+" ");
+				
+			}
+			System.out.println();
+			break;
+		case 3:
+			for(int i+0 i<pedejie.size(); i++){
+				if(pedejie.get(i)>lielakais){
+					lielakais = pedejie.get(i);
+				}
+			}
+		break;
+		case 4:
+			System.out.println("aptureta ");
+			break;
+		
+		}
+		
+		
+		}while(izvele!=4);
+		
+		
 	
 		scan.close();
 	}
